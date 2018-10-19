4cabf3199801fee94747978549411684a4042581
Markus Sabadello <markus@danubetech.com>
2018-10-19 17:59:35 +0200

    WIP

--- drivers/v1/src/main/java/uniregistrar/driver/did/v1/DidV1Driver.java
+++ drivers/v1/src/main/java/uniregistrar/driver/did/v1/DidV1Driver.java
@@ -1,6 +1,10 @@
 package uniregistrar.driver.did.v1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -12,6 +16,7 @@ import uniregistrar.request.RegisterRequest;
 import uniregistrar.request.RevokeRequest;
 import uniregistrar.request.UpdateRequest;
 import uniregistrar.state.RegisterState;
+import uniregistrar.state.RegisterStateFinished;
 import uniregistrar.state.RevokeState;
 import uniregistrar.state.UpdateState;
 
@@ -70,10 +75,6 @@ public class DidV1Driver implements Driver {
 	@Override
 	public RegisterState register(RegisterRequest registerRequest) throws RegistrationException {
 
-		// open
-
-		if (this.getTrustAnchorSeed() == null) this.openIndy();
-
 		// read parameters
 
 		String network = registerRequest.getOptions() == null ? null : (String) registerRequest.getOptions().get("network");
@@ -81,7 +82,61 @@ public class DidV1Driver implements Driver {
 
 		// register
 
-		throw new RuntimeException("Not implemented.");
+		BufferedReader reader;
+
+		try {
+
+			Process process = Runtime.getRuntime().exec("did generate  --register");
+			process.waitFor();
+			reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		} catch (IOException | InterruptedException ex) {
+
+			throw new RegistrationException("Cannot generate DID: " + ex.getMessage(), ex);
+		}
+
+		String newDid;
+
+		try {
+
+			newDid = reader.readLine();
+		} catch (IOException ex) {
+
+			throw new RegistrationException("Cannot read DID: " + ex.getMessage(), ex);
+		}
+
+		String newPrivateKey;
+
+		try {
+
+			newPrivateKey = reader.readLine();
+		} catch (IOException ex) {
+
+			throw new RegistrationException("Cannot read private key: " + ex.getMessage(), ex);
+		}
+
+		// create METHOD METADATA
+
+		Map<String, Object> methodMetadata = new LinkedHashMap<String, Object> ();
+		methodMetadata.put("network", network);
+
+		// create IDENTIFIER
+
+		String identifier = "did:sov:";
+		if (network != null && ! network.isEmpty() && ! network.equals("_")) identifier += network + ":";
+		identifier += newDid;
+
+		// create CREDENTIALS
+
+		Map<String, Object> credentials = new LinkedHashMap<String, Object> ();
+		credentials.put("privateKey", newPrivateKey);
+
+		// create REGISTER STATE
+
+		RegisterState registerState = new RegisterStateFinished(null, null, methodMetadata, identifier, credentials);
+
+		// done
+
+		return registerState;
 	}
 
 	@Override
@@ -100,10 +155,6 @@ public class DidV1Driver implements Driver {
 	public Map<String, Object> properties() {
 
 		return this.getProperties();
-	}
-
-	private void openIndy() throws RegistrationException {
-
 	}
 
 	/*
