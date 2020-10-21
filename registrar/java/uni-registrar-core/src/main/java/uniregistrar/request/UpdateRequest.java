ce538631a5cfdd2f4d8479753c339964aa9ac2cd
Markus Sabadello <markus@danubetech.com>
2020-10-21 14:11:15 +0200

    fix: Upgrade to latest did-common-java.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- registrar/java/uni-registrar-core/src/main/java/uniregistrar/request/UpdateRequest.java
+++ registrar/java/uni-registrar-core/src/main/java/uniregistrar/request/UpdateRequest.java
@@ -11,8 +11,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import did.DIDDocument;
+import foundation.identity.did.DIDDocument;
 
 public class UpdateRequest {
 
