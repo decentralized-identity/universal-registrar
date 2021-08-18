98f842815f3d55253cad873bbbbcca708fa13e00
Markus Sabadello <markus@danubetech.com>
2021-08-18 12:00:27 +0400

    refactor: Small update to extensions interface.

--- uni-registrar-local/src/main/java/uniregistrar/local/extensions/Extension.java
+++ uni-registrar-local/src/main/java/uniregistrar/local/extensions/Extension.java
@@ -11,49 +11,30 @@ import uniregistrar.state.UpdateState;
 
 public interface Extension {
 
-	public ExtensionStatus beforeCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
-	public ExtensionStatus beforeUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
-	public ExtensionStatus beforeDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
-	public ExtensionStatus afterCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
-	public ExtensionStatus afterUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
-	public ExtensionStatus afterDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException;
+	public default ExtensionStatus beforeCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
+
+	public default ExtensionStatus beforeUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
+
+	public default ExtensionStatus beforeDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
+
+	public default ExtensionStatus afterCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
+
+	public default ExtensionStatus afterUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
+
+	public default ExtensionStatus afterDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+		return null;
+	}
 
 	public abstract static class AbstractExtension implements Extension {
-
-		@Override
-		public ExtensionStatus beforeCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
-
-		@Override
-		public ExtensionStatus beforeUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
-
-		@Override
-		public ExtensionStatus beforeDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
-
-		@Override
-		public ExtensionStatus afterCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
-
-		@Override
-		public ExtensionStatus afterUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
-
-		@Override
-		public ExtensionStatus afterDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
-
-			return null;
-		}
 	}
 }
--- uni-registrar-local/src/main/java/uniregistrar/local/extensions/impl/DummyExtension.java
+++ uni-registrar-local/src/main/java/uniregistrar/local/extensions/impl/DummyExtension.java
@@ -2,12 +2,11 @@ package uniregistrar.local.extensions.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import uniregistrar.RegistrationException;
 import uniregistrar.local.LocalUniRegistrar;
 import uniregistrar.local.extensions.Extension;
-import uniregistrar.local.extensions.ExtensionStatus;
 import uniregistrar.local.extensions.Extension.AbstractExtension;
+import uniregistrar.local.extensions.ExtensionStatus;
 import uniregistrar.request.CreateRequest;
 import uniregistrar.state.CreateState;
 
@@ -15,10 +14,10 @@ public class DummyExtension extends AbstractExtension implements Extension {
 
 	private static Logger log = LoggerFactory.getLogger(DummyExtension.class);
 
+	@Override
 	public ExtensionStatus afterCreate(String method, CreateRequest createRequest, CreateState createState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
 
 		if (log.isDebugEnabled()) log.debug("Dummy extension called!");
-
 		return ExtensionStatus.DEFAULT;
 	}
 }
