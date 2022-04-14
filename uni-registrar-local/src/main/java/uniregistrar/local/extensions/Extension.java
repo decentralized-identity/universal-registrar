0906fb37c5c936f6b7fe8965203a5b641dfc2742
Markus Sabadello <markus@danubetech.com>
2022-04-14 16:41:04 +0200

    feat: Add convenience superclass for extensions that can handle all requests.

--- uni-registrar-local/src/main/java/uniregistrar/local/extensions/Extension.java
+++ uni-registrar-local/src/main/java/uniregistrar/local/extensions/Extension.java
@@ -4,9 +4,11 @@ import uniregistrar.RegistrationException;
 import uniregistrar.local.LocalUniRegistrar;
 import uniregistrar.request.CreateRequest;
 import uniregistrar.request.DeactivateRequest;
+import uniregistrar.request.Request;
 import uniregistrar.request.UpdateRequest;
 import uniregistrar.state.CreateState;
 import uniregistrar.state.DeactivateState;
+import uniregistrar.state.State;
 import uniregistrar.state.UpdateState;
 
 import java.util.Map;
@@ -38,5 +40,40 @@ public interface Extension {
 	}
 
 	abstract class AbstractExtension implements Extension {
+	}
+
+	abstract class AbstractRequestExtension extends AbstractExtension implements Extension {
+
+		public final ExtensionStatus beforeCreate(String method, CreateRequest createRequest, CreateState createState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.beforeRequest(method, createRequest, createState, executionState, localUniRegistrar);
+		}
+
+		public final ExtensionStatus beforeUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.beforeRequest(method, updateRequest, updateState, executionState, localUniRegistrar);
+		}
+
+		public final ExtensionStatus beforeDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.beforeRequest(method, deactivateRequest, deactivateState, executionState, localUniRegistrar);
+		}
+
+		public final ExtensionStatus afterCreate(String method, CreateRequest createRequest, CreateState createState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.afterRequest(method, createRequest, createState, executionState, localUniRegistrar);
+		}
+
+		public final ExtensionStatus afterUpdate(String method, UpdateRequest updateRequest, UpdateState updateState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.afterRequest(method, updateRequest, updateState, executionState, localUniRegistrar);
+		}
+
+		public final ExtensionStatus afterDeactivate(String method, DeactivateRequest deactivateRequest, DeactivateState deactivateState, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return this.afterRequest(method, deactivateRequest, deactivateState, executionState, localUniRegistrar);
+		}
+
+		public ExtensionStatus beforeRequest(String method, Request request, State state, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return null;
+		}
+
+		public ExtensionStatus afterRequest(String method, Request request, State state, Map<String, Object> executionState, LocalUniRegistrar localUniRegistrar) throws RegistrationException {
+			return null;
+		}
 	}
 }
